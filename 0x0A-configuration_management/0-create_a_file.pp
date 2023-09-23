36fd2cf8549a04afe6e3a5f983338cf8b4554772
XimeonLeo <ximeonleo@gmail.com>
2023-09-23 02:25:15 +0100

    configuration management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 # creating a file /tmp/school using Puppet
+
 file { '/tmp/school':
+  content => 'I love Puppet',
   mode    => '0774',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
 }

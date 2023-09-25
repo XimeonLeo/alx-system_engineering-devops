12a2191567ea2daf77c36e9e979da108d71f05f0
XimeonLeo <ximeonleo@gmail.com>
2023-09-25 03:44:48 +0100

    writing a manifest to change configuration file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -10,7 +10,7 @@ file_line { 'Disabling Passwd Auth':
 
 file_line { 'Setting identity file':
   ensure  => present,
-  path    => '~/etc/ssh/ssh_config',
+  path    => '/etc/ssh/ssh_config',
   line    => '     IdentityFile ~/.ssh/school',
   replace => true,
 }

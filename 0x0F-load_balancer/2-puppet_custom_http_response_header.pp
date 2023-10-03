25529f69db836225e63af2a14f0f2979302c7d6b
XimeonLeo <ximeonleo@gmail.com>
2023-10-03 12:54:13 +0100

    automating installation

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,30 @@
+# Automating the configuration of Nginx on a new Ubuntu
+#machine
+
+# updating the ubuntu server
+exec { 'update ubuntu':
+  command  => 'apt-get update',
+  user     => 'root',
+  provider => 'shell',
+}
+->
+# ensure that nginx is installed
+package { 'nginx_server':
+  ensure   => present,
+  provider => 'apt'
+}
+->
+# Adding a Nginx response header (X-Served-By: hostname)
+file_line { 'add HTTP header':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'add_header X-Served-By $hostname;'
+}
+->
+# start service
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx']
+}
